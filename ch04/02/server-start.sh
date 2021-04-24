65fd6ce7dc3b411a6a127e2d8a88d19c5ca3536e
aws-bitacademy <aws.bitacademy@gmail.com>
2021-04-24 14:14:53 +0900

    start server script

---
+++ ch04/02/server-start.sh
@@ -0,0 +1,29 @@
+#! /bin/bash -e
+
+echo '[1] Get the ID of Amazon Linux AMI...'
+AMIID=$(aws ec2 describe-images --query "Images[?Description=='Amazon Linux 2 AMI 2.0.20210326.0 x86_64 HVM gp2'].ImageId" --output 
+text)
+
+echo '[2] Get the default VPC ID...'
+VPCID=$(aws ec2 describe-vpcs --query "Vpcs[?isDefault==true].VpcId" --output text)
+
+echo '[3] Get the default subnet ID...'
+SUBNETID=$(aws ec2 describe-subnets --query "(Subnets[?VpcId=='"$VPCID"'].SubnetId)[0]" --output text)
+
+echo '[4] Create a security group...'
+SGID=$(aws ec2 create-security-group --group-name mysecuritygroup --description "My Security Group" --vpc-id vpc-27cb4b4c --output text)
+
+echo '[5] Allow inbound SSH connections...'
+aws ec2 authorize-security-group-ingress --group-id $SGID --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+echo '[6] Create and start the server...'
+INSTANCEID=$(aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --security-group-ids $SGID --subnet-id $SUBNETID --key-name mykey --query "Instances[0].InstanceId" --output text)
+
+echo "waiting for $INSTANCEID..."
+aws ec2 wait instance-running --instance-ids $INSTANCEID
+
+echo '[7] Get the public name of server...'
+PUBLICNAME=$(aws ec2 describe-instances --instance-ids $INSTANCEID --query "Reservations[0].Instances[0].PublicDnsName" --output text)
+
+echo "$INSTANCEID is accepting SSH connections under $PUBLICNAME"
+echo "ssh -i mykey.pem ec2-user@$PUBLICNAME"
